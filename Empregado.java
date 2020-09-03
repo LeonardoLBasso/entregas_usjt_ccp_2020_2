d2b472e0d3da4fcd496632e368e9932eeb37f64c
Leonardo Lara Basso <leolara0811@gmail.com>
2020-09-03 20:37:14 -0300

    secondary release, added what I think is the salary calculation the teacher implied in his class

--- Empregado.java
+++ Empregado.java
@@ -13,4 +13,20 @@ public class Empregado{
     public void setNome(String nome) {
         this.nome = nome;
     }
+    private double salary;
+    private double bonus;
+    private double comission;
 }
+public double calcSalary{
+    switch (tipo){
+        case 1:
+            return salary;
+        case 2:
+            return salary + salary*comission;
+        case 3:
+            return salary + comission + bonus;
+        default:
+            return 0;
+    }
+}
+ 
