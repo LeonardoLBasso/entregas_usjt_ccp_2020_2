4d8ef03228ef21c6fb9bc96118656534f2eb06f5
Leonardo Lara Basso <leolara0811@gmail.com>
2020-09-03 20:50:07 -0300

    third release, please god end this

--- Empregado.java
+++ Empregado.java
@@ -1,32 +1,48 @@
-public class Empregado{
+import java.lang.reflect.Constructor;
+
+public class Empregado {
     private String nome;
     private int idade;
-    public int getIdade() {
-        return idade;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+    private int tipo;
     private double salary;
     private double bonus;
     private double comission;
-}
-public double calcSalary{
-    switch (tipo){
-        case 1:
-            return salary;
-        case 2:
-            return salary + salary*comission;
-        case 3:
-            return salary + comission + bonus;
-        default:
-            return 0;
+
+    public Empregado (String nome, int idade, int tipo, double salary, double bonus, double comission){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+        this.salary = salary;
+        this.bonus = bonus;
+        this.comission = comission;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double calcSalary() {
+        switch (tipo) {
+            case 1:
+                return salary;
+            case 2:
+                return salary + salary * comission;
+            case 3:
+                return salary + comission + bonus;
+            default:
+                return 0;
+        }
     }
 }
- 
---
+++ TesteEmpregado.java
@@ -0,0 +1,7 @@
+public class TesteEmpregado {
+    public static void main(String[] args){
+       Empregado e1 = new Empregado("Diego", 19, 3, 2000, 200, 15);
+       Empregado e2 = new Empregado("Analice", 50, 1, 5000, 0, 0);
+       Empregado e3 = new Empregado("Tyler", 20, 2, 3000, 0, 20); 
+    }
+}
